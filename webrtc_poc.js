15700ad2ef0a6ea7b9051151b5dd452e5878c599
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2014-02-27 21:36:33 +0100

    Simple UI rework.

--- webrtc_poc.js
+++ webrtc_poc.js
@@ -3,46 +3,42 @@ window.URL = window.URL || window.webkitURL;
 
 var stream_handle = null
 var microphone = null
-var filter = null
 var recorder = null
-
-function success_callback(stream) {
-    console.log("Successfully enabled microphone.")
-
-    $("#mic").removeClass("turned-off")
-    $("#mic").addClass("turned-on")
-
-    $("#mic > i").removeClass("fa-microphone-slash")
-    $("#mic > i").addClass("fa-microphone")
-
-    $("#start-recording").removeAttr("disabled")
-
-    $("#allow-mic-hint").css("text-decoration", "line-through")
-
-    stream_handle = stream
-}
-
-function error_callback(error) {
-    console.log("Error: microphone wasn't enabled.", error)
-
-    $("#mic").removeClass("turned-on")
-    $("#mic").addClass("turned-off")
-
-    $("#mic > i").removeClass("fa-microphone")
-    $("#mic > i").addClass("fa-microphone-slash")
-
-    $("#start-recording").attr("disabled", "disabled")
-
-    $("#allow-mic-hint").css("text-decoration", "none")
-
-    stream_handle = null
-}
+var audio_context = null
 
 if (getUserMedia) {
     console.log("getUserMedia() is supported in your browser")
 
+    function userMedia_success(stream) {
+        console.log("Successfully enabled microphone.")
+
+        $("#mic").removeClass("turned-off")
+        $("#mic").addClass("turned-on")
+
+        $("#mic > i").removeClass("fa-microphone-slash")
+        $("#mic > i").addClass("fa-microphone")
+
+        $("#start-recording").removeAttr("disabled")
+
+        stream_handle = stream
+    }
+
+    function userMedia_error(error) {
+        console.log("Error: microphone wasn't enabled.", error)
+
+        $("#mic").removeClass("turned-on")
+        $("#mic").addClass("turned-off")
+
+        $("#mic > i").removeClass("fa-microphone")
+        $("#mic > i").addClass("fa-microphone-slash")
+
+        $("#start-recording").attr("disabled", "disabled")
+
+        stream_handle = null
+    }
+
     $(document).ready(function() {
-        getUserMedia({audio: true}, success_callback, error_callback)
+        getUserMedia({audio: true}, userMedia_success, userMedia_error)
     })
 } else {
     console.log("getUserMedia() is not supported!")
@@ -50,43 +46,53 @@ if (getUserMedia) {
 
 if (window.AudioContext) {
     console.log("AudioContext is supported in your browser!")
+    audio_context = new AudioContext()
 
     $("#start-recording").click(function() {
-        // this init could be moved to other init callback
-        context = new AudioContext()
-        microphone = context.createMediaStreamSource(stream_handle)
-
+        microphone = audio_context.createMediaStreamSource(stream_handle)
         recorder = new Recorder(microphone, {workerPath: "recorderWorker.js"})
         // no need to connect to speakers. Only recording.
-        // microphone.connect(context.destination)
+        // microphone.connect(audio_context.destination)
         recorder && recorder.record()
 
         $("#start-recording").toggleClass("hidden")
         $("#stop-recording").toggleClass("hidden")
-        $("#start-recording-hint").css("text-decoration", "line-through")
 
         console.log("Recording started")
+
+        li = document.createElement("li")
+        i_el = document.createElement("i")
+        i_el.className = "fa fa-circle"
+        li.appendChild(document.createTextNode("recording "))
+        li.appendChild(i_el)
+        recordings.appendChild(li)
     })
 
     $("#stop-recording").click(function() {
         $("#start-recording").toggleClass("hidden")
         $("#stop-recording").toggleClass("hidden")
-        $("#start-recording-hint").css("text-decoration", "none")
         recorder.stop()
         recorder.exportWAV(function(blob) {
-            var url = URL.createObjectURL(blob);
-            var li = document.createElement('li');
-            var audio_element = document.createElement('audio');
-            var anchor = document.createElement('a');
+            url = URL.createObjectURL(blob)
 
-            audio_element.controls = true;
-            audio_element.src = url;
-            anchor.href = url;
-            anchor.download = new Date().toISOString() + '.wav';
-            anchor.innerHTML = "Download";
-            li.appendChild(audio_element);
-            li.appendChild(anchor);
-            recordings.appendChild(li);
+            li = document.getElementById("recordings").lastChild
+
+            audio_element = document.createElement('audio')
+            audio_element.controls = true
+            audio_element.src = url
+
+            anchor = document.createElement('a')
+            anchor.href = url
+            anchor.download = new Date().toISOString() + '.wav'
+            anchor.innerHTML = "Download recording"
+
+            li.removeChild(li.lastChild)
+            li.removeChild(li.lastChild)
+
+            li.appendChild(audio_element)
+            li.appendChild(document.createElement("br"))
+            li.appendChild(anchor)
+            recordings.appendChild(li)
         })
 
         console.log("Recording stopped")
